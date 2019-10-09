8371ad9c782f02444ef4efe71b120f3819acdf8d
pilgyumin <pilgyu12@naver.com>
2019-10-09 20:01:17 +0900

    심규민 멘토님 코드 리팩토링

--- src/routes/control_Machine/airconditioner_Control.js
+++ src/routes/control_Machine/airconditioner_Control.js
@@ -2,41 +2,19 @@ import express from 'express'
 const http = require('http');
 const router = express.Router();
 
-let pi_server_Url = {
-    hostname: '192.168.0.9',
-    port: '3000',
-    path : '/Airconditioner_Control/'
-};
+const hostname = '192.168.0.9'
+const port = '3000'
+const path = '/Airconditioner_Control/'
 
-/* GET home page. */
-router.post('/power', (req, res, next) => {
-    const aa = {};
-    console.log('airconditioner power');
-    pi_server_Url.path += "power";
-    http.request(pi_server_Url).end();
-    console.log(pi_server_Url);
-    pi_server_Url.path = '/Airconditioner_Control/';
-    res.json(aa);
-});
-
-router.post('/speedup', (req, res, next) => {
-    const aa = {};
-    console.log('airconditioner speedup');
-    pi_server_Url.path += "speedup";
-    http.request(pi_server_Url).end();
-    console.log(pi_server_Url);
-    pi_server_Url.path = '/Airconditioner_Control/';
-    res.json(aa);
-});
-
-router.post('/speeddown', (req, res, next) => {
-    const aa = {};
-    console.log('airconditioner speeddown');
-    pi_server_Url.path += "speeddown";
-    http.request(pi_server_Url).end();
-    console.log(pi_server_Url);
-    pi_server_Url.path = '/Airconditioner_Control/';
-    res.json(aa);
+router.post('/:action', (req, res, next) => {
+    console.log(`airconditioner ${req.params.action}`);
+    http.request({
+        hostname,
+        port,
+        path: path + req.params.action
+    }).end();
+    console.log(path + req.params.action);
+    res.json({});
 });
 
 module.exports = router;
