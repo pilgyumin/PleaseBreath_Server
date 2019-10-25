cac19483d8817782c07ec6137ac7d9721af10bcb
조현학 <zhh102000@gmail.com>
2019-10-25 16:51:27 +0900

    [Run38][control_Machine/airconditioner_Control   power, speedup, tempup, tempdown 명령 Pi로 전송][서버 개발 에어컨 리모컨 작동]

--- src/routes/control_Machine/airconditioner_Control.js
+++ src/routes/control_Machine/airconditioner_Control.js
@@ -4,7 +4,49 @@ const router = express.Router();
 
 const hostname = '192.168.0.9'
 const port = '3000'
-const path = '/Airconditioner_Control/'
+const path = '/AirconditionerControl/'
+
+let pi_server_Url = {
+    hostname: '192.168.0.9',
+    port: '3000',
+    path : '/AirconditionerControl/'
+};
+
+router.post('/power', (req, res, next) => {
+    const aa = {};
+    console.log('Airconditioner power');
+    pi_server_Url.path += "power";
+    http.request(pi_server_Url).end();
+    pi_server_Url.path = '/AirconditionerControl/';
+    res.json(aa);
+});
+
+router.post('/speedup', (req, res, next) => {
+    const aa = {};
+    console.log('Airconditioner speed');
+    pi_server_Url.path += "speedup";
+    http.request(pi_server_Url).end();
+    pi_server_Url.path = '/AirconditionerControl/';
+    res.json(aa);
+});
+
+router.post('/tempup', (req, res, next) => {
+    const aa = {};
+    console.log('Airconditioner tempup');
+    pi_server_Url.path += "tempup";
+    http.request(pi_server_Url).end();
+    pi_server_Url.path = '/Airconditioner_Control/';
+    res.json(aa);
+});
+
+router.post('/tempdown', (req, res, next) => {
+    const aa = {};
+    console.log('Airconditioner tempdown');
+    pi_server_Url.path += "tempdown";
+    http.request(pi_server_Url).end();
+    pi_server_Url.path = '/Airconditioner_Control/';
+    res.json(aa);
+});
 
 router.post('/:action', (req, res, next) => {
     console.log(`airconditioner ${req.params.action}`);
