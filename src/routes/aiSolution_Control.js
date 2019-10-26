7bb82c08d7d27d9fb2e914517053e1e08f11b6a6
ujahnn <ujahnn@gmail.com>
2019-10-26 19:36:53 +0900

    [not_issue][routes/aiSolution_Control][change pi ip,ai_control url]

--- src/routes/aiSolution_Control.js
+++ src/routes/aiSolution_Control.js
@@ -5,30 +5,27 @@ const http = require('http');
 let solution_status = require('../Model/aiSolution_status');
 
 let pi_server_Url={
-    hostname: '192.168.0.9',
+    hostname: '192.168.1.84',
     port: '3000',
-    path : '/aiSolutionControl/'
+    path : '/Mode/Normal'
 };
 
 router.get('/ON', (req, res, next) => {
     solution_status.power = 1;
     console.log('aiSolution ON');
-    pi_server_Url.path += "ON";
     http.request(pi_server_Url).end();
     console.log(pi_server_Url);
-    pi_server_Url.path = '/aiSolutionControl/';
+    pi_server_Url.path = '/Mode/Normal';
     res.json({});
 });
 
 router.get('/OFF', (req, res, next) => {
     solution_status.power = 0;
     console.log('aiSolution OFF');
-    pi_server_Url.path += "OFF";
     http.request(pi_server_Url).end();
     console.log(pi_server_Url);
-    pi_server_Url.path = '/aiSolutionControl/';
+    pi_server_Url.path = '/Mode/Normal';
     res.json({});
 });
 
 module.exports = router;
-
