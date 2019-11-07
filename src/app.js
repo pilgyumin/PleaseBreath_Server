60f1273ab342c2ccc12727edb68885f78b66cf44
pilgyumin <pilgyu12@naver.com>
2019-11-07 21:52:36 +0900

    [RUN-125][app/js, routes/reservation_control.js][ [기능 개발] 예약 모드 (서비스 로직 수정 필요)] - 논리 추가로 구현해야됨 아직 미완성

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- src/routes/reservation_control.js
+++ src/routes/reservation_control.js
@@ -4,40 +4,36 @@ import express from 'express'
 const router = express.Router()
 const http = require('http');
 
-var schedule = require('node-schedule');
-
 let solution_status = require('../Model/aiSolution_status');
 
-const pi_hostname = '192.168.1.2';
+//const pi_hostname = '192.168.1.84';
+const pi_hostname = 'localhost';
 const pi_port = '3000';
-const pi_path = '/aiSolutionControl/';
-
-let job;
+const pi_path = '/ReservationControl/';
 
 router.post('/ON', (req, res, next) => {
     console.log('reservation ON');
+    console.log(req.body);
+    let data = {
+        year : req.body.year,
+        month : req.body.month,
+        day : req.body.day,
+        hour : req.body.hour,
+        minute : req.body.minute,
+        second : req.body.second
+    }
+    http.request({
+        hostname : pi_hostname,
+        port : pi_port,
+        path: pi_path + 'ON',
+        method: 'POST',
+        headers: {
+            'Content-Type': ' application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(JSON.stringify(data))
+        },
+    }).end();
 
-    let date = new Date(req.body.year, req.body.month, req.body.day, req.body.hour, req.body.minute, req.body.second);
-
-    job = schedule.scheduleJob(date, function(){
-        console.log('schedule start');
-        solution_status.power = 1;
-        http.request({
-            hostname : pi_hostname,
-            port : pi_port,
-            path: pi_path + 'ON'
-        }).end();
-    });
-
-    res.json({});
-});
-
-router.post('/OFF', (req, res, next) => {
-    solution_status.power = 0;
-    console.log('reservation OFF');
-    job.cancel();
-    res.json({});
+    res.json(JSON.stringify(req.body));
 });
 
 module.exports = router;
-
