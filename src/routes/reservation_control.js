9048bdafe8c8ffc7d764278e4d7f2eab06fa682a
pilgyumin <pilgyu12@naver.com>
2019-11-12 16:12:55 +0900

    [RUN-122, RUN-125][/routes/resertation_control.js][추가 기능 [모드 설정 페이지] 꺼짐 예약 설정 기능, 기능 개발 [기능 개발] 예약 모드 (서비스 로직 수정 필요)] - 두 이슈를 한 라우터 안에 동시에 해결

--- src/routes/reservation_control.js
+++ src/routes/reservation_control.js
@@ -3,36 +3,35 @@
 import express from 'express'
 const router = express.Router()
 const http = require('http');
+const fetch = require('node-fetch');
 
 let solution_status = require('../Model/aiSolution_status');
+let request = require('request');
 
 //const pi_hostname = '192.168.1.84';
 const pi_hostname = 'localhost';
 const pi_port = '3000';
 const pi_path = '/ReservationControl/';
 
-router.post('/ON', (req, res, next) => {
-    console.log('reservation ON');
-    console.log(req.body);
-    let data = {
-        year : req.body.year,
-        month : req.body.month,
-        day : req.body.day,
-        hour : req.body.hour,
-        minute : req.body.minute,
-        second : req.body.second
-    }
-    http.request({
-        hostname : pi_hostname,
-        port : pi_port,
-        path: pi_path + 'ON',
-        method: 'POST',
-        headers: {
-            'Content-Type': ' application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(JSON.stringify(data))
-        },
-    }).end();
+// 요청이 들어오면
+// data set
+// res.redirect('url' + resource)
 
+
+router.post('/:mode/:power', (req, res, next) => {
+    // 솔루션 가동 예약 명령
+
+    let reservation_data = {
+        data : new Date(req.body.year, req.body.month - 1, req.body.day, req.body.hour, req.body.minute , 0 ).getTime() / 1000
+    };
+
+    fetch("http://" + pi_hostname + ":" + pi_port + pi_path + req.params.mode + '/' + req.params.power, {
+        method: 'post',
+        body:    JSON.stringify(reservation_data),
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(res => res.json())
+        .then(json => console.log(json));
     res.json(JSON.stringify(req.body));
 });
 
